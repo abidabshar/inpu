7f2ab88d556b6eb196e079c6724c6536f8ddd9eb
root <root@localhost>
2022-03-19 09:49:18 +0600

    auto publish 1647661757243

---
+++ main.py
@@ -0,0 +1,16 @@
+#input everything
+name=input("who is your best friend:")
+print ("Best Friend:"+name)
+
+
+#input number full from
+
+phone_number=input ("Phone number:")
+only=int(phone_number)
+print (only)
+
+
+
+#input number short from
+cell=int(input('What are your cell number?'))
+print (cell)
